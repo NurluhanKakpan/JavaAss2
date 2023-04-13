bf3f550b89e0a696634245629205efe976c08a3c
Your Name <you@example.com>
2023-04-13 11:20:40 +0600

    Created a new class for LinkedList

---
+++ src/MyLinkedList.java
@@ -0,0 +1,71 @@
+public class MyLinkedList implements MyList{
+
+    private int size;
+
+    private class Node<E>{
+
+        E data;
+        Node<E> next;
+        Node<E> previous;
+        public Node(E data){
+            this.data = data;
+        }
+    }
+    private Node head;
+    private Node tail;
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
